699554d5d6fe3339069c4bb37d4e8e3aae7f632d
Christian Peraza <christianperaza2000@gmail.com>
2024-09-12 09:53:32 -0300

    listGoals function created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require("@inquirer/prompts");
+const { select, input, checkbox } = require("@inquirer/prompts");
 
 let goals = [];
 
@@ -21,6 +21,47 @@ const createGoal = async () => {
             checked: false
         }
     )
+
+}
+
+const listGoals = async () => {
+
+    if(goals.length == 0) {
+        console.log("The list is empty.");
+        return
+    } else {
+        const answers = await checkbox(
+            {
+                message: "Use the arrows to move, or space to select or unselect, or enter to exit this stage.",
+                choices: [...goals],
+                instructions: false
+            }
+        )
+
+        if(answers.length == 0) {
+            console.log("No goal was selected.");
+
+            goals.forEach((g) => {
+                g.checked = false;
+            })
+            
+            return
+        }
+
+        goals.forEach((g) => {
+            g.checked = false;
+        })
+    
+        answers.forEach((answer) => {
+            const goal = goals.find((g) => {
+                return g.value == answer
+            })
+    
+            goal.checked = true;
+        })
+
+        console.log("Goal(s) successfully checked.");
+    }
 
 }
 
@@ -51,7 +92,7 @@ const start = async () => {
                 await createGoal()
                 break
             case "list":
-                console.log("Let's list")
+                await listGoals()
                 break
             case "exit":
                 console.log("See you later!")
