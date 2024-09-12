55ea98c227dd4faa477fb7dee8e0f29a3ebc0729
Christian Peraza <christianperaza2000@gmail.com>
2024-09-11 23:10:07 -0300

    Options menu initialized

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const start = () => {
+const { select } = require("@inquirer/prompts");
+
+const start = async () => {
     
     while(true) {
 
-        const option = "Exit";
+        const option = await select({
+            message: "Choose an option >",
+            choices: [
+                {
+                    name: "Track goal",
+                    value: "track"
+                },
+                {
+                    name: "List goals",
+                    value: "list"
+                },
+                {
+                    name: "Exit",
+                    value: "exit"
+                }
+            ]
+        });
 
         switch(option) {
-            case "Track goal":
+            case "track":
                 console.log("Let's track")
                 break
-            case "List goals":
+            case "list":
                 console.log("Let's list")
                 break
-            case "Exit":
+            case "exit":
                 console.log("See you later!")
                 return
         }
